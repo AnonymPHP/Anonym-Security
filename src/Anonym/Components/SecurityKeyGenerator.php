d63470d659977eee27cbfa6b14b58d6c135ef760
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-28 00:32:41 +0300

    first update for security key generator

--- src/Anonym/Components/SecurityKeyGenerator.php
+++ src/Anonym/Components/SecurityKeyGenerator.php
@@ -13,11 +13,25 @@
      * Class SecurityKeyGenerator
      * @package Anonym\Components\Security
      */
-    class SecurityKeyGenerator
+    class SecurityKeyGenerator implements KeyGeneratorInterface
     {
-
-        public function __construct(){
-
+        /**
+         * Güvenlik kodunu oluşturur
+         *
+         * @return string
+         */
+        public function generate(){
+            $ip = $_SERVER['REMOTE_ADDR'];
+            $len = strlen($ip);
+            $letters = [];
+            for ($i = 'a', $j = 1; $j <= 26; $i++, $j++) {
+                $letters[$j] = $i;
+            }
+            $bas = substr($ip, 0, 2);
+            $con = $letters[$len];
+            $son = substr($ip, $len - 1, 1);
+            $con2 = $letters[$len.$son];
+            $key = $son  . $con . $con2 . $ip . $bas;
+            return md5($key);
         }
-
     }
