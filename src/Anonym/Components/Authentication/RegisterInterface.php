7da2580d14d209662ab5dbf930b700dcbab049d3
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-28 23:38:22 +0300

    added register interface

---
+++ src/Anonym/Components/Authentication/RegisterInterface.php
@@ -0,0 +1,19 @@
+<?php
+    /**
+     * Bu Dosya AnonymFramework'e ait bir dosyadır.
+     *
+     * @author vahitserifsaglam <vahit.serif119@gmail.com>
+     * @see http://gemframework.com
+     *
+     */
+
+    namespace Anonym\Components\Security\Authentication;
+
+    /**
+     * Interface RegisterInterface
+     * @package Anonym\Components\Security\Authentication
+     */
+    interface RegisterInterface
+    {
+
+    }
