ac37350cdb1f8a744f4e3cee63dee5cc1aa18c74
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-28 19:23:13 +0300

    added AuthenticationMatcherInterface interface

--- src/Anonym/Components/Authentication.php
+++ src/Anonym/Components/Authentication.php
@@ -9,11 +9,82 @@
 
     namespace Anonym\Components\Security;
 
+    use Anonym\Components\Cookie\Cookie;
+    use Anonym\Components\Cookie\CookieInterface;
+    use Anonym\Components\Session\Session;
+    use Anonym\Components\Session\SessionInterface;
+
     /**
      * Class Authentication
      * @package Anonym\Components\Security
      */
     class Authentication
     {
+
+        /**
+         * Session objesini tutar
+         *
+         * @var SessionInterface
+         */
+        private $session;
+
+        /**
+         * Cookie objesini tutar
+         *
+         * @var CookieInterface
+         */
+        private $cookie;
+
+        private $matcher;
+
+        const USER_SESSION = 'AnonymFrameworkUser';
+
+        /**
+         * Sınıfı başlatır ve session ve cookie objelerini atar
+         */
+        public function __construct(){
+            $this->setSession( new Session());
+            $this->setCookie( new Cookie());
+        }
+
+        /**
+         * @return SessionInterface
+         */
+        public function getSession()
+        {
+            return $this->session;
+        }
+
+        /**
+         * @param SessionInterface $session
+         * @return Authentication
+         */
+        public function setSession(SessionInterface $session)
+        {
+            $this->session = $session;
+
+            return $this;
+        }
+
+        /**
+         * @return CookieInterface
+         */
+        public function getCookie()
+        {
+            return $this->cookie;
+        }
+
+        /**
+         * @param CookieInterface $cookie
+         * @return Authentication
+         */
+        public function setCookie(CookieInterface $cookie)
+        {
+            $this->cookie = $cookie;
+
+            return $this;
+        }
+
+
 
     }
