655f9f104a15a3cb6fb5e49d88adf889afa51c82
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-28 23:37:55 +0300

    added register class

---
+++ src/Anonym/Components/Authentication/Register.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     * Bu Dosya AnonymFramework'e ait bir dosyadır.
+     *
+     * @author vahitserifsaglam <vahit.serif119@gmail.com>
+     * @see http://gemframework.com
+     *
+     */
+
+    namespace Anonym\Components\Security\Authentication;
+    /**
+     * Class Register
+     * @package Anonym\Components\Security\Authentication
+     */
+    class Register extends Authentication
+    {
+
+    }
