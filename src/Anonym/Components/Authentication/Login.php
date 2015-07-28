a5ebb0c3bca8cf4e61f31857a4140924674e739d
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-28 21:30:13 +0300

    added login interface

--- src/Anonym/Components/Authentication/Login.php
+++ src/Anonym/Components/Authentication/Login.php
@@ -9,8 +9,70 @@
 
     namespace Anonym\Components\Security\Authentication;
 
-
-    class Login
+    /**
+     * Class Login
+     * @package Anonym\Components\Security\Authentication
+     */
+    class Login extends Authentication
     {
+
+        /**
+         * Veritabanı objesini tutar
+         *
+         * @var AuthenticationDatabaseReadInterface
+         */
+        private $db;
+
+        /**
+         * @param AuthenticationDatabaseReadInterface $db
+         */
+        public function __construct(AuthenticationDatabaseReadInterface $db){
+            parent::__construct();
+            $this->setDb($db);
+        }
+
+        /**
+         * Kullanıcı girişi yaptırılır
+         * $remember true girilirse cookie e atanır veriler.
+         *
+         * @param string $username
+         * @param string $password
+         * @param bool|false $remember
+         * @return bool
+         */
+        public function login($username = '', $password = '', $remember = false)
+        {
+
+        }
+
+        /**
+         * @return AuthenticationDatabaseReadInterface
+         */
+        public function getDb()
+        {
+            return $this->db;
+        }
+
+        /**
+         * @param AuthenticationDatabaseReadInterface $db
+         * @return Login
+         */
+        public function setDb($db)
+        {
+            $this->db = $db;
+
+            return $this;
+        }
+
+        /**
+         * Dinamik olarak method çağrımı
+         *
+         * @param $method
+         * @param $params
+         * @return mixed
+         */
+        public function __call($method, $params){
+            return call_user_func_array([$this->getDb(), $method], $params);
+        }
 
     }
