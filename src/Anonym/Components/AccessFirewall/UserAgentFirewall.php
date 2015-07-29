1d18f3e942e94b551df19f75b1bda9e6a5391bce
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-29 16:59:31 +0300

    update useragent firewall

--- src/Anonym/Components/AccessFirewall/UserAgentFirewall.php
+++ src/Anonym/Components/AccessFirewall/UserAgentFirewall.php
@@ -9,7 +9,39 @@
 
     namespace Anonym\Components\Security;
 
-    class UserAgentFirewall
+    /**
+     * Class UserAgentFirewall
+     * @package Anonym\Components\Security
+     */
+    class UserAgentFirewall implements FirewallCheckerInterface
     {
 
+        /**
+         * İzin verilen değerleri tutar
+         *
+         * @var mixed
+         */
+        private $alloweds;
+
+        /**
+         * useragent değerini tutar
+         *
+         * @var string
+         */
+        private $userAgent;
+
+        public function __construct($alloweds, $useragent){
+            $this->alloweds = $alloweds;
+            $this->userAgent = $useragent;
+        }
+
+        /**
+         * Karşılaştırma yapar
+         *
+         * @return bool
+         */
+        public function handle()
+        {
+
+        }
     }
