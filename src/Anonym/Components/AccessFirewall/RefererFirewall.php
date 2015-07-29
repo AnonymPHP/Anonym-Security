f5f936bd6bd5f681667295bf7d65d682d05ee711
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-30 00:49:24 +0300

    updated referer firewall class

--- src/Anonym/Components/AccessFirewall/RefererFirewall.php
+++ src/Anonym/Components/AccessFirewall/RefererFirewall.php
@@ -9,12 +9,20 @@
 
     namespace Anonym\Components\Security;
 
+    use Anonym\Components\Security\FirewallCheckerInterface;
     /**
      * Class RefererFirewall
      * @package Anonym\Components\Security
      */
-    class RefererFirewall
+    class RefererFirewall extends FirewallChecker implements FirewallCheckerInterface
     {
-
+        /**
+         * Kontrol işlemini gerçekleştirir
+         *
+         * @return bool
+         */
+        public function handle(){
+            return $this->defaultChecker();
+        }
     }
 
