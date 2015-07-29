fff523506dfa659b158e4247b23540d36a99c31a
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-30 00:36:14 +0300

    added accept firewall and updated language firewall

--- src/Anonym/Components/AccessFirewall/LanguageFirewall.php
+++ src/Anonym/Components/AccessFirewall/LanguageFirewall.php
@@ -8,12 +8,35 @@
      */
 
     namespace Anonym\Components\Security;
-
+    use Anonym\Components\Security\FirewallCheckerInterface;
     /**
      * Class LanguageFirewall
      * @package Anonym\Components\Security
      */
-    class LanguageFirewall
+    class LanguageFirewall extends FirewallChecker implements FirewallCheckerInterface
     {
+
+        /**
+         * Kontrol işlemini gerçekleştirir
+         *
+         * @return bool
+         */
+        public function handle(){
+            $allowed = $this->getAlloweds();
+            $useragent = $this->getValue();
+            if('*' === $allowed){
+                return true;
+            }
+
+            $allowed = (array) $allowed;
+            if (is_array($allowed)) {
+                foreach($allowed as $allow)
+                {
+                    if (strstr($useragent, $allow)) {
+                        return true;
+                    }
+                }
+            }
+        }
 
     }
