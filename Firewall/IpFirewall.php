25fb298145811d40f66ddf19aa8fb02d0a771c7f
anonymframework <serif.saglam119@gmail.com>
2015-08-25 07:08:24 +0300

    reformatted

--- Firewall/IpFirewall.php
+++ Firewall/IpFirewall.php
@@ -1,43 +1,47 @@
 <?php
-    /**
-     * Bu Dosya AnonymFramework'e ait bir dosyadır.
-     *
-     * @author vahitserifsaglam <vahit.serif119@gmail.com>
-     * @see http://gemframework.com
-     *
-     */
+/**
+ * Bu Dosya AnonymFramework'e ait bir dosyadır.
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ */
 
-       namespace Anonym\Components\Security\Firewall;
-    use Anonym\Components\Security\Exception\FirewallException;
-    use M6Web\Component\Firewall\Firewall as ParentFirewall;
+namespace Anonym\Components\Security\Firewall;
+
+use Anonym\Components\Security\Exception\FirewallException;
+use M6Web\Component\Firewall\Firewall as ParentFirewall;
+
+/**
+ * Class Firewall
+ * @package Anonym\Components\Security
+ */
+class IpFirewall extends ParentFirewall implements FirewallCheckerInterface
+{
 
     /**
-     * Class Firewall
-     * @package Anonym\Components\Security
+     * sınıfı başlatır
      */
-    class IpFirewall extends ParentFirewall implements FirewallCheckerInterface
+    public function __construct()
     {
-
-        /**
-         * sınıfı başlatır
-         */
-        public function __construct(){
-            parent::__construct();
-        }
-
-        /**
-         * Yakalama işlemini yapar veya exception oluşturur
-         *
-         * @return bool
-         */
-        public function handle(){
-            return parent::handle([$this, 'getException']);
-        }
-
-        /**
-         * Exception oluşturur
-         */
-        public function getException(){
-            throw new FirewallException('Giriş yaptığınız adres, güvenlik duvarımıza takılmıştır.');
-        }
+        parent::__construct();
     }
+
+    /**
+     * Yakalama işlemini yapar veya exception oluşturur
+     *
+     * @return bool
+     */
+    public function handle()
+    {
+        return parent::handle([$this, 'getException']);
+    }
+
+    /**
+     * Exception oluşturur
+     */
+    public function getException()
+    {
+        throw new FirewallException('Giriş yaptığınız adres, güvenlik duvarımıza takılmıştır.');
+    }
+}
