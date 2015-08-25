e854c0bef388b70826771c3ba54345e7a08a2b40
anonymframework <serif.saglam119@gmail.com>
2015-08-25 07:08:07 +0300

    fixed a little bug

--- Firewall/IpFirewall.php
+++ Firewall/IpFirewall.php
@@ -15,7 +15,7 @@
      * Class Firewall
      * @package Anonym\Components\Security
      */
-    class Firewall extends ParentFirewall implements FirewallCheckerInterface
+    class IpFirewall extends ParentFirewall implements FirewallCheckerInterface
     {
 
         /**
